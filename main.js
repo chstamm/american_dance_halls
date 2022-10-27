cf6eed8adbfbd349c5c6d6e9de28d61f891d8c3f
Chelsea Stamm <stamm.chelsea@gmail.com>
2022-10-26 20:22:11 -0600

    fixed nav

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 //Mobile Nav
 
 function setUpNav() {
+  let nav = document.querySelector(".sidenav");
   let buttons = document.querySelectorAll(".nav-toggle");
   buttons.forEach((element) => {
-    let nav = document.querySelector(".sidenav");
+    
     element.addEventListener("click", () => {
       if (nav.classList.contains("open")) nav.classList.remove("open");
       else nav.classList.add("open");
     
     });
   });
-  let links = document.getElementsByTagName ("a");
+  let links = document.querySelectorAll ("nav a");
   links.forEach((link) => {
-    link.addEventListener("click", 
+    link.addEventListener("click", () => {
       nav.classList.remove("open")
+    }
 )
   })
 }
