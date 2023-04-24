69e313ddb283f210b75b442cea7f35ca38ef1df4
Chelsea Stamm <stamm.chelsea@gmail.com>
2023-04-24 12:40:34 -0600

    Troubleshooting Image Gallery

--- main.js
+++ main.js
@@ -4,43 +4,41 @@ function setUpNav() {
   let nav = document.querySelector(".sidenav");
   let buttons = document.querySelectorAll(".nav-toggle");
   buttons.forEach((element) => {
-    
     element.addEventListener("click", () => {
       if (nav.classList.contains("open")) nav.classList.remove("open");
       else nav.classList.add("open");
-    
     });
   });
-  let links = document.querySelectorAll ("nav a");
+  let links = document.querySelectorAll("nav a");
   links.forEach((link) => {
     link.addEventListener("click", () => {
-      nav.classList.remove("open")
-    }
-)
-  })
+      nav.classList.remove("open");
+    });
+  });
 }
 
 setUpNav();
 
-
 // Image Carousal
 
-const scrollLeftBtn = document.getElementById("scroll-left");
-const scrollRightBtn = document.getElementById("scroll-right");
-const galleryWrapper = document.querySelector(".gallery-wrapper");
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollLeftBtn = document.querySelector("#scroll-left");
+  const scrollRightBtn = document.querySelector("#scroll-right");
+  const galleryWrapper = document.querySelector(".gallery-wrapper");
 
-scrollLeftBtn.addEventListener("click", () => {
-  galleryWrapper.scrollBy(-300, 0);
-});
-
-scrollRightBtn.addEventListener("click", () => {
-  galleryWrapper.scrollBy(300, 0);
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") {
+  scrollLeftBtn.addEventListener("click", () => {
     galleryWrapper.scrollBy(-300, 0);
-  } else if (event.key === "ArrowRight") {
+  });
+
+  scrollRightBtn.addEventListener("click", () => {
     galleryWrapper.scrollBy(300, 0);
-  }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      galleryWrapper.scrollBy(-300, 0);
+    } else if (event.key === "ArrowRight") {
+      galleryWrapper.scrollBy(300, 0);
+    }
+  });
 });
